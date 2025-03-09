147d2ea580959308bf0ebc46480bf3d945c75a31
Felix Korankye Adu <adukorankye16@gmail.com>
2025-03-09 13:53:55 +0000

    changing of font color of login and register screen to black

---
+++ frontend/app/profile.tsx
@@ -0,0 +1,10 @@
+// app/(tabs)/profile.js
+import { View, Text } from 'react-native';
+
+export default function ProfileScreen() {
+  return (
+    <View>
+      <Text>Profile Page</Text>
+    </View>
+  );
+}
--- frontend/app/register.tsx
+++ frontend/app/register.tsx
@@ -127,13 +127,13 @@ const styles = StyleSheet.create({
     padding: 16,
   },
   logoText: {
-    fontSize: 32,
-    color: '#fff',
+    fontSize: 28,
+    color: '#000',
     fontWeight: 'bold',
   },
   title: {
-    fontSize: 24,
-    color: '#fff',
+    fontSize: 20,
+    color: '#000',
     textAlign: 'center',
     marginBottom: 22,
   },
@@ -151,7 +151,7 @@ const styles = StyleSheet.create({
     width: '100%',
     height: 48,
     backgroundColor: '#000',
-    borderRadius: 8,
+    borderRadius: 30,
     justifyContent: 'center',
     alignItems: 'center',
     marginVertical: 16,
@@ -165,7 +165,7 @@ const styles = StyleSheet.create({
     marginTop: 16,
   },
   loginText: {
-    color: '#fff',
+    color: '#000',
   },
   logoImage: {
     width: 150,
