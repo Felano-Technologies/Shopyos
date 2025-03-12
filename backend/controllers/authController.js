f32bd3b3c6d4b1e0f8d9748f617faeb406bb7535
Felix Korankye Adu <adukorankye16@gmail.com>
2025-03-12 01:11:41 +0000

    `Added fullPhoneNumber to User model and updated authController.js to include it in user registration and login responses.`

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -5,7 +5,7 @@ const nodemailer = require('nodemailer');
 const crypto = require('crypto');
 
 const register = async (req, res) => {
-  const { name, email, password } = req.body;
+  const { name, email, password, fullPhoneNumber } = req.body;
   try {
     let user = await User.findOne({ email });
 
@@ -16,6 +16,7 @@ const register = async (req, res) => {
     user = new User({
       name,
       email,
+      fullPhoneNumber,
       password
     });
 
@@ -73,17 +74,12 @@ const login = async (req, res) => {
       user: {
         // id: user._id,
         name: user.name,
-<<<<<<< HEAD
-        email: user.email
-      }
-=======
         email: user.email,
         phone: user.fullPhoneNumber,
         latitude: user.latitude,
         longitude: user.longitude
       },
       message : "Login successful",
->>>>>>> 28d5c67ab758ba77510975397156e3367696b2aa
     });
   } catch (err) {
     res.status(400).json({ error: err.message });
